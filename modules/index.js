c71b5e314f3351aaaf4e7686871180a391b80e6b
prashant <prashantkirpan96@gmail.com>
2023-03-03 14:31:52 +0530

    initial commit

---
+++ modules/index.js
@@ -0,0 +1,12 @@
+let a= "prashant";
+console.log(a);
+const add = require("./add")
+
+
+const sum = add(2,3);
+console.log(sum);
+
+const sub = require("./subs")
+
+const substraction = sub(2,1);
+console.log(substraction);
