49c483f8a8de76ff8cea23868aaa3b64364261f2
prashant <prashantkirpan96@gmail.com>
2023-03-05 17:41:18 +0530

    change

--- express/index.js
+++ express/index.js
@@ -1,20 +1,35 @@
-const express = require('express');
+const express = require("express");
+const { join } = require("path");
 const path = require("path");
 const app = express();
-const port = 3000;
+const port = 3001;
 
-const pathdir = path.join(__dirname,'public')
-
+const pathdir = path.join(__dirname,'views')
 app.use(express.static(pathdir));
 
-app.get('/', (req, res) =>{
-     res.sendFile(`${pathdir}/index.html`);
-    });
+app.set("view engine", "ejs");
+console.log(app.get("view engine"));
+console.log(path.join(__dirname), "views");
 
-app.get('/about', (req, res) =>{
-    res.sendFile(`${pathdir}/home.html`)});
+app.get("/", (req, res) => {
+  //  res.sendFile(`${pathdir}/index.html`);
+  res.render("index",{
+    title:"index"
+  })
+});
 
-app.get('/download', (req, res) =>{
-    res.download(`${pathdir}/index.html`)});
+app.get("/about", (req, res) => {
+  // res.sendFile(`${pathdir}/home.html`)
+  res.render("home",{
+    title:"home"
+});      
+}); 
 
-app.listen(port, () => {console.log(`Example app listening on port ${port}!`)});
+app.get("/download", (req, res) => {
+    res.download(`${pathdir}/index.ejs`)
+
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`);
+});
